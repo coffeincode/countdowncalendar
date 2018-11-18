5698094b049970565a9001da0c564c2fd1739094
Marina Diezler <md@diezler.net>
2018-11-18 15:46:00 +0100

    bundle information was missing

---
+++ src/CoffeinCodeCountdownCalendarBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CoffeinCode\CountdownCalendarBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the Contao CountdownCalendar bundle.
+ *
+ * @author Marina Diezelr
+ */
+class CoffeinCodeCountdownCalendarBundle extends Bundle
+{
+
+}
+
